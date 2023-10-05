45b3ef5187e19c216fda88461b98068753174dc5
Boldinov <pashaboldinov@gmail.com>
2023-10-05 20:02:37 +0500

    Start

---
+++ src/main/java/org/example/enums/NegativeProperties.java
@@ -0,0 +1,12 @@
+package org.example.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum NegativeProperties {
+
+    private String name;
+    private String influence;
+    private int price;
+    private Rarity rarity;
+}
