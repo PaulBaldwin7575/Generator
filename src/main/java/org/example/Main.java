fe226d1ad70a42a327f3410e0100ae07c3dd3720
Boldinov <pashaboldinov@gmail.com>
2023-10-08 18:27:15 +0500

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Введите уровень игрока(от 1 до 100):");
+        var scanner = new Scanner(System.in);
+        var level = scanner.nextInt();
         for (int i = 0; i < 20; i++) {
-            var chance = (int) (Math.random() * 100);
+            var chance = (int) (Math.random() * 100 + level/4);
             Rarity rarity = Rarity.getRarity(chance);
             System.out.println(rarity.getName() + chance);
         }
--- src/main/java/org/example/enums/NegativeProperties.java
+++ src/main/java/org/example/enums/NegativeProperties.java
@@ -5,6 +5,15 @@ import lombok.Getter;
 @Getter
 public enum NegativeProperties {
 
+    ATTACK("", "Атака", 1, Rarity.ORDINARY);
+
+    NegativeProperties(String name, String influence, int price, Rarity rarity) {
+        this.name = name;
+        this.influence = influence;
+        this.price = price;
+        this.rarity = rarity;
+    }
+
     private String name;
     private String influence;
     private int price;
